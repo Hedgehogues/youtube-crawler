2122a8ba22523eef21f9624665d5e6a3891f3f95
Урванов Егор <urvanov@corp.sputnik.ru>
2019-01-28 02:25:11 +0300

    Push-commit master

--- crawler/xxx.py
+++
@@ -1,9 +0,0 @@
-from youtube_dl.extractor import gen_extractor_classes
-from youtube_dl.extractor.common import InfoExtractor
-
-infoExtractor = InfoExtractor()
-downloader = gen_extractor_classes()[1126]
-infoExtractor.set_downloader(downloader)
-x = infoExtractor.extract('LJ1s8Esuyfw')
-
-print(x)
