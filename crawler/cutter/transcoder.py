df033a7a937652d13b220e00da1b4602a10f2131
Урванов Егор <urvanov@corp.sputnik.ru>
2019-02-06 16:19:02 +0300

    Add args for mock-object

--- crawler/cutter/transcoder.py
+++ crawler/cutter/transcoder.py
@@ -5,7 +5,7 @@ from webvtt import WebVTT
 import html
 from pysrt.srtitem import SubRipItem
 from pysrt.srttime import SubRipTime
-from crawler.cutter import validate_ext, srt_ext, vtt_ext
+from crawler.cutter import validate_ext
 
 
 class FfmpegWavTranscoder:
@@ -23,13 +23,15 @@ class FfmpegWavTranscoder:
 # Требования:
 # * Преобразование vtt в srt
 # * Возможность очистки vtt
-# * Задавать кастомное имя и путь до srt-файла
 # * Задавать суффикс для всех srt-файлов
 class VttToSrtTranscoder:
-    def __init__(self, suffix=''):
+    def __init__(self, suffix='', vtt_reader=None):
         self.__suffix = suffix
-        self.__ext = 'srt'
-        self.__vtt_reader = WebVTT()
+        self.__out_ext = 'srt'
+        self.__in_ext = 'vtt'
+        self.__vtt_reader = vtt_reader
+        if self.__vtt_reader is None:
+            self.__vtt_reader = WebVTT()
 
     def __write_subs(self, out_fd, in_path):
         for index, caption in enumerate(self.__vtt_reader.read(in_path)):
@@ -38,27 +40,21 @@ class VttToSrtTranscoder:
             item = SubRipItem(index+1, start, end, html.unescape(caption.text))
             out_fd.write("%s\n" % str(item))
 
-    def __create_out_filename(self, in_file_path, out_vtt_path):
-        out_vtt_path = os.path.abspath(out_vtt_path)
-
-        out_file_path = "%s.%s" % (in_file_path, self.__ext)
+    def __create_out_filename(self, in_file_path):
+        out_file_path = "%s.%s" % (in_file_path, self.__out_ext)
         if len(self.__suffix) > 0:
-            out_file_path = "%s-%s.%s" % (in_file_path, self.__suffix, self.__ext)
-
-        if len(out_vtt_path) > 0:
-            out_path = validate_ext(out_vtt_path, vtt_ext)
-            out_file_path = "%s.%s" % (self.__ext, out_path)
+            out_file_path = "%s-%s.%s" % (in_file_path, self.__suffix, self.__out_ext)
 
         return out_file_path
 
-    def apply(self, in_vtt_path, out_vtt_path='', clear=True):
+    def apply(self, in_vtt_path, clear=True):
         # *_file_path -- path to file without extension (e.t. *, for example 'subs')
         # *_vtt_path -- path to file with vtt-extension (e.t. *.vtt, for example 'subs.vtt')
 
         in_vtt_path = os.path.abspath(in_vtt_path)
-        in_file_path = validate_ext(in_vtt_path, vtt_ext)
+        in_file_path = validate_ext(in_vtt_path,  self.__in_ext)
 
-        out_file_path = self.__create_out_filename(in_file_path, out_vtt_path)
+        out_file_path = self.__create_out_filename(in_file_path)
 
         with open(out_file_path, "w") as out_fd:
             self.__write_subs(out_fd, in_vtt_path)
