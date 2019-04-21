046dbb228ce9270d8755fe6e407bdc2cafca533e
Urvanov Egor <eurvanov@ozon.ru>
2019-04-21 21:26:44 +0300

    Fix max

--- main.py
+++ main.py
@@ -10,20 +10,5 @@ channel_ids = ['UCzAzPC4VWIMHqrnIM1iBPsQ']
 x = YoutubeCrawler(cache=DBSqlLiteCache(hard=True), max_attempts=1)
 x.process(channel_ids)
 
-
-loader = Loader()
-reloader = Reloader()
-ydl_loader = YoutubeDlLoader()
-scrapper = Scrapper(
-    loader, reloader, ydl_loader,
-    [
-        parsers.HomePageParser(),
-        parsers.VideosParser(max_page=10),
-        parsers.ChannelsParser(max_page=3),
-        parsers.AboutParser(),
-    ]
-)
-descr = scrapper.parse(channel_ids)
-# scrapper.download(descr[Tab.Videos][1]['video_id'])
-print(descr)
+print('Finish')
 
